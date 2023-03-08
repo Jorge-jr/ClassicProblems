42ea03391a7b77affad8f9a2727a810445df1c8f
Jorge-jr <jsribeirojr@hotmail.com>
2023-03-08 13:11:57 -0300

    added fibbonacci

---
+++ fibonecci/fibonecci.py
@@ -0,0 +1,17 @@
+from typing import Generator
+
+
+def fib(n: int) -> Generator[int, None, None]:
+    yield 0
+    if n > 0:
+        yield 1
+    last: int = 0
+    next: int = 1
+    for _ in range(1, n):
+        last, next = next, next + last
+        yield next
+
+
+if __name__ == "__main__":
+    for i in fib(60):
+        print(i)
