4dd7a507b69b63d81296357a6b10cb3de68283c2
Jorge-jr <jsribeirojr@hotmail.com>
2023-07-26 00:00:11 -0300

    added decrypt finction to unbreakeble_encryption.py

--- Chapter 1/unbreakeble_encryption.py
+++ Chapter 1/unbreakeble_encryption.py
@@ -6,6 +6,7 @@ def random_key(length: int) -> int:
     tb: bytes = token_bytes(length)
     return int.from_bytes(tb, "big")
 
+
 def encrypt(original: str) -> Tuple[int, int]:
     original_bytes: bytes = original.encode()
     dummy: int = random_key(len(original_bytes))
@@ -13,4 +14,18 @@ def encrypt(original: str) -> Tuple[int, int]:
     encrypted: int = original_key ^ dummy  # XOR
     return dummy, encrypted
 
-#TODO: DECRYPT function
+
+def decrypt(key1: int, key2: int) -> str:
+    decripted: int = key1 ^ key2
+    temp: bytes = decripted.to_bytes((decripted.bit_length()+7)//8)
+    return temp.decode()
+
+
+if __name__ == "__main__":
+    key1, key2 = encrypt("One time Pad!")
+    print(f"key 1: {key1}, key 2: {key2}")
+    result: str = decrypt(key1, key2)
+    print(result)
+
+
+# TODO: DECRYPT function
